fd4ff1451693773b6d91fda84962863f130d85f0
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-05-13 10:12:43 +0530

    remove spacing in  contact us page

--- src/ui/sections/ContactUs.tsx
+++ src/ui/sections/ContactUs.tsx
@@ -4,7 +4,7 @@ import GradientButton from '../../components/inputs/GradientButton'
 
 const ContactUs = () => {
     return (
-        <section className='container sm:min-h-screen py-10 max-w-7xl px-10 '>
+        <section className='container  pt-10 pb-14 max-w-7xl px-10 '>
             <h1 className='text-4xl leading-[3rem] max-md:text-center md:text-5xl md:leading-[3.75rem] lg:text-6xl xl:text-7xl lg:!leading-[4.5rem] font-[500] text-center bg-primary_100 bg-clip-text text-transparent lg:px-20'>Contact us
             </h1>
             <p className='text-[#8C8888] text-center py-6 text-lg lg:text-2xl font-normal '>Connect with us share your thoughts</p>
