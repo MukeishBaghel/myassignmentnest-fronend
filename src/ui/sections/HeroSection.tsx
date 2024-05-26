e1b5758a7c385cc8691b30a16918bc521a86b2e3
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-05-26 11:46:56 +0530

    fix herso section

--- src/ui/sections/HeroSection.tsx
+++ src/ui/sections/HeroSection.tsx
@@ -3,13 +3,14 @@ import heroLady from '/assets/images/herogirl.gif'
 import sitejabbar from '/assets/images/sitejabber.svg'
 import trustpilot from '/assets/images/trustpilot.svg'
 import { Link } from 'react-router-dom'
+import heroimg from '/assets/images/heroimg.svg'
 
 const HeroSection = () => {
     return (
-        <section className='container lg:px-20 mt-10'>
-            <div className='flex items-center justify-between overflow-hidden'>
-                <div className='xl:pr-10 '>
-                    <h1 className='mt-10 text-4xl leading-[3rem] max-md:text-center md:text-5xl md:leading-[4rem] lg:text-7xl lg:!leading-[5.25rem] font-[500]'>
+        <section className='container lg:px-20 '>
+            <div className=' flex items-center justify-between'>
+                <div className='w-full md:w-2/3 lg:w-1/2 pt-5'>
+                    <h1 className='pt-10 text-4xl leading-[3rem] max-md:text-center md:text-5xl md:leading-[4rem] lg:text-7xl lg:!leading-[5.25rem] font-[500]'>
                         <span className='text-primary-100'> Hey there</span>
                         <span className='bg-primary_100 bg-clip-text text-transparent'>,<br />ready to unlock your academic potential?</span>
                         <div className='flex items-center gap-4 sm:-ml-4 w-full max-md:justify-center mt-4'>
@@ -18,7 +19,9 @@ const HeroSection = () => {
                         </div>
                     </h1>
                 </div >
-                <img src={heroLady} alt="assignment helper" className='hidden md:block md:w-[43%] lg:w-[45%]' />
+                <div className='hidden md:block'>
+                    <img src={heroimg} alt="assignment helper" className='' />
+                </div>
             </div>
         </section >
     )
