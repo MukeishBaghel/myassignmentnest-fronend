828f4ad4711f58a29b43be87b53fa077a9aed375
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2024-05-31 11:14:25 +0530

    fix footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Button from '../inputs/Button'
 import { Link } from 'react-router-dom'
-import { Facebook, Instagram, Linkedin } from 'lucide-react'
+import { Facebook, Instagram, Linkedin, Mail, Phone, PhoneCall, PhoneIcon } from 'lucide-react'
 import gatewayimg from '/assets/images/gatewayimg.jpeg'
 
 const Footer = () => {
   return (
-    <footer className='bg-primary_100 p-4 sm:p-12 '>
-      <section className=' grid grid-cols-2  gap-y-6 sm:gap-14 lg:grid-cols-4 justify-items-center place-content-between align-middle  text-primary'>
+    <footer className='bg-primary_100 p-2 sm:p-12 '>
+      <section className=' grid grid-cols-2   gap-y-6 sm:gap-10 md:gap-14 xl:grid-cols-4 justify-items-center place-content-between align-middle  text-primary'>
         <div className='flex flex-col gap-6 '>
           <h1 className='text-xl sm:text-2xl md:text-3xl font-semibold'>
             Ready to get started?
@@ -25,7 +25,7 @@ const Footer = () => {
           {/* <p className='text-sm pr-4 max-sm:hidden'>Disclaimer: The reference papers provided by MyAssignmentnest.com serve as model papers for students and are not to be submitted as it is. These papers are intended to be used for research and reference purposes only.</p> */}
 
         </div>
-        <div className='flex flex-col gap-2 max-sm:text-sm max-lg:justify-self-end'>
+        <div className='flex flex-col gap-2 max-sm:text-sm max-xl:justify-self-end'>
           <h1 className='font-semibold text-lg '>Our Policy</h1>
           <Link to={"/privacy-policy"} className='hover:opacity-50 duration-150 ease-in  '>
             Privacy Policy
@@ -41,16 +41,16 @@ const Footer = () => {
           </Link>
 
         </div>
-        <div className='flex flex-col gap-2 max-sm:text-sm '>
-          <h1 className='font-semibold text-lg text-start text-nowrap'>Contact us</h1>
-          <Link to={"mailto:support@myassignmentnest.com"}>
-            Email:
-            <span className='hover:opacity-50 duration-150 ease-in '>
+        <div className='flex flex-col gap-2 max-sm:text-sm max-xl:justify-self-start'>
+          <h1 className='font-semibold text-lg text-start '>Contact us</h1>
+          <Link to={"mailto:support@myassignmentnest.com"} className='flex items-center gap-1 flex-wrap'>
+            <Mail className='w-4 h-4'/>:
+            <span className='hover:opacity-50 duration-150 ease-in truncate max-sm:max-w-[160px]'>
               support@myassignmentnest.com
             </span>
           </Link>
-          <Link to={'tel:+918439223922'} >
-            Phone: <span className='hover:opacity-50 duration-150 ease-in '>+91 8439223922</span>
+          <Link to={'tel:+918439223922'} className='flex items-center gap-1' >
+            <Phone className='w-4 h-4'/>: <span className='hover:opacity-50 duration-150 ease-in '>+91 8439223922</span>
           </Link>
         </div>
         <div className='flex flex-col gap-4 justify-between max-sm:text-sm justify-self-end'>
@@ -66,7 +66,7 @@ const Footer = () => {
               Free Tools
             </a>
           </div>
-          <div className=''><p className='italic'>100% Secure Payment</p> <img src={gatewayimg} className='w-28 lg:w-48' alt="" /></div>
+          <div className=''><p className='italic'>100% Secure Payment</p> <img src={gatewayimg} className='w-28 lg:w-40' alt="" /></div>
         </div>
 
       </section>
